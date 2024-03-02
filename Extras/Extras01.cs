05fe97cd89cdf7a08e66fc6e8d50565e70c8887e
Vitor Moutim <moutimg@gmail.com>
2024-03-02 09:02:59 -0300

    feat: cria desafio extra 01

--- Extras/Extras01.cs
+++ Extras/Extras01.cs
@@ -7,6 +7,25 @@ namespace desafios_csharp.Extras
 {
     public class Extras01
     {
-        
+        public static void EstadoAguaTemperatura() {
+            Console.WriteLine("Digite uma temperatura em celsius para saber estado de físico da água: ");
+            float temperatura = float.Parse(Console.ReadLine());
+
+            string estado = "";
+
+            if (temperatura < 0) {
+                estado = "sólido";
+            } else if (temperatura == 0) {
+                estado = "ponto de fusão";
+            } else if (temperatura > 0 && temperatura < 100) {
+                estado = "liquído";
+            } else if (temperatura == 100) {
+                estado = "ponto de ebulição";
+            } else {
+                estado = "gasoso";
+            }
+
+            Console.WriteLine($"O estado da água é: {estado}");
+        }
     }
 }
