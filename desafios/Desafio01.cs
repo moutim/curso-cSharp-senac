f142d0ff598c24f89ecc5a48be5d0648e532c095
Vitor Moutim <moutimg@gmail.com>
2024-03-02 08:44:20 -0300

    fix: corrige diretorio do namespace

--- desafios/Desafio01.cs
+++ desafios/Desafio01.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace desafios_csharp.desafios
+namespace desafios_csharp.Desafios
 {
     public class Desafio01
     {
