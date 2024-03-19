f85048f0e2a2461b1eb0ba67d0952f5734541d43
Vitor Moutim <moutimg@gmail.com>
2024-03-19 18:29:21 -0300

    fix: arruma caminho do namespace

--- Desafios/Desafio03.cs
+++ Desafios/Desafio03.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace desafios_csharp.Desafios
+namespace Desafios_csharp.Desafios
 {
     public class Desafio03
     {
